4aa6a100b02842da6b05ace8fa02b6a6838d53a3
loster12520 <131502697+loster12520@users.noreply.github.com>
2023-09-27 10:43:42 +0800

    2023.09.27 10.42 补全src/Test、src/ui/Control的注释

--- src/Test.java
+++ src/Test.java
@@ -4,12 +4,14 @@ import model.Resource;
 import ui.Control;
 import ui.Frame;
 import ui.GameImage;
-
 import java.awt.image.BufferedImage;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
-
+/**
+ * Test
+ * 临时的启动器，以后会考虑制作一个功能更全面的来代替
+ */
 public class Test {
     private Resource resource;
     private GameImage gameImage;
@@ -19,21 +21,34 @@ public class Test {
     private Control control;
 
     public Test() throws IOException {
+        //资源包根路径
         String path="C:/code/new_2048/resources/lingting/";
+        //gson对象
         Gson gson=new Gson();
         Reader reader=new FileReader(path+"config.json");
+        //解析数据
         resource=gson.fromJson(reader,Resource.class);
+        //获取图片逻辑类
         gameImage=new GameImage(resource,path);
+        //新建游戏逻辑
         game=new Game(4);
+        //获取图片
         bufferedImage=gameImage.getImage(game.getChecks());
+        //新建窗口
         frame=new Frame(bufferedImage);
+        //注入监视器
         control=new Control(frame, game, new Control.Flushable() {
             public void flush() {
+                //重新获取图片
                 bufferedImage=gameImage.getImage(game.getChecks());
+                //刷新
                 frame.repaint(bufferedImage);
             }
         });
     }
+    /**
+     * 平平无奇的主函数，可惜的是没加上异常捕获
+     */
     public static void main(String[] args) throws Exception {
         new Test();
     }
--- src/ui/Control.java
+++ src/ui/Control.java
@@ -1,21 +1,47 @@
+/**
+ * 用于注入动作监视器的类，很简单的代码
+ * 姑且丢在ui包下吧
+ */
 package ui;
-
+/**
+ * 导包
+ */
 import model.Game;
-
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-
+/**
+ * @author loster12520
+ */
 public class Control {
+    /**
+     * 窗口类索引
+     */
     private Frame frame;
+    /**
+     * 逻辑类索引
+     */
     private Game game;
+    /**
+     * 定义实现flush的方式的格式
+     */
     public static interface Flushable{
         void flush();
     }
+    /**
+     * 实现flush的方式
+     */
     private Flushable flushable;
+    /**
+     * 初始化
+     * @param frame 配套的窗口对象
+     * @param game 配套的游戏逻辑对象
+     * @param flushable 配套的刷新方式
+     */
     public Control(Frame frame, Game game,Flushable flushable) {
         this.frame = frame;
         this.game = game;
         this.flushable=flushable;
+        //注入键盘监测器
         frame.addKeyListener(new KeyListener() {
             public void keyTyped(KeyEvent e) {
                 switch (e.getKeyChar()){
@@ -32,10 +58,13 @@ public class Control {
                     case 'r'->
                         game.initGame();
                 }
+                //
                 System.out.println(e.getKeyCode()+" "+e.getKeyChar());
                 game.print();
+                //刷新
                 flushable.flush();
             }
+            //空置函数
             public void keyPressed(KeyEvent e) {}
             public void keyReleased(KeyEvent e) {}
         });
