abfce2a044929f08258e5818d82593174a2be86c
loster12520 <131502697+loster12520@users.noreply.github.com>
2023-09-27 09:19:04 +0800

    2023.09.27  9.18 src/model/Game全部注释补齐

--- src/model/Game.java
+++ src/model/Game.java
@@ -105,21 +105,30 @@ public class Game {
      * 创建新方块
      */
     private void createCheck() {
+        //获取空白方格列表
         List<Integer[]> list = getEmptyChecks();
-
+        //判定是否需要创建新方块
         if (!list.isEmpty() && is_change) {
+            //获取随机位置
             int index = random.nextInt(list.size());
+            //获取坐标
             Integer i=list.get(index)[0],j = list.get(index)[1];
-            // 2, 4出现概率9:1
+            // 随机一个数字2/4  2, 4出现概率9:1
             int randomValue = random.nextInt(10);
+            //注入数字
             checks[i][j] = randomValue==0 ? 4 : 2;//只有[0,4)中的2才能生成4
 //            is_change = false;
         }
     }
 
-    // 获取空白方格
+    /**
+     * 获取空白方格列表
+     * @return 空白方格列表
+     */
     private List<Integer[]> getEmptyChecks() {
+        //新建列表
         List<Integer[]> checkList = new ArrayList<>();
+        //遍历面板数据，若为空则计入列表
         for (int i = 0; i < length; i++) {
             for (int j = 0; j < length; j++) {
                 if (checks[i][j] == 0) {
@@ -127,15 +136,20 @@ public class Game {
                 }
             }
         }
+        //返回列表
         return checkList;
     }
-    //是否全部格子占满，全部占满则GameOver
+    /**
+     * 是否全部格子占满，全部占满则GameOver<br>
+     * 本方法暂时并没有用到
+     * @return 是否
+     */
     private boolean judgeGameOver() {
-
+        //仍有空格则未结束
         if (!getEmptyChecks().isEmpty()) {
             return false;
         }
-
+        //遍历，半段是否有相邻两项相等，有则未结束
         for (int i = 0; i < length-1; i++) {
             for (int j = 0; j < length-1; j++) {
                 //判断是否存在可合并的方格
@@ -145,10 +159,14 @@ public class Game {
                 }
             }
         }
+        //这下真结束了
         return true;
     }
-
+    /**
+     * 向左移动
+     */
     public void moveLeft() {
+        //若改变过，保存一遍副本
         if(is_change)
             save();
         //找到一个非空格子后checks[i][j].value > 0，可分为三种情况处理
@@ -157,7 +175,6 @@ public class Game {
                 if (checks[i][j] > 0) {
                     //第一种情况：checks[i][j]（非第1列）与checks[i][index]的数相等，则合并乘以2，且得分增加
                     if (checks[i][j].equals(checks[i][index])) {
-                        System.out.println("!!!");
                         score += checks[i][index] *= 2;
                         checks[i][j] = 0;
                         is_change = true;
@@ -175,9 +192,12 @@ public class Game {
                 }
             }
         }
+        //新增一个方块喵～
         createCheck();
     }
-
+    /**
+     * 向右移动
+     */
     public void moveRight() {
         if(is_change)
             save();
@@ -202,7 +222,9 @@ public class Game {
         }
         createCheck();
     }
-
+    /**
+     * 向上移动
+     */
     public void moveUp() {
         if(is_change)
             save();
@@ -227,7 +249,9 @@ public class Game {
         }
         createCheck();
     }
-
+    /**
+     * 向下移动
+     */
     public void moveDown() {
         if(is_change)
             save();
@@ -252,20 +276,27 @@ public class Game {
         }
         createCheck();
     }
+    /**
+     * 保存一下副本，将上一次的那啥储存起来
+     */
     private void save(){
         is_change = false;
         for (int i=0;i<length;i++)
             for (int j=0;j<length;j++)
                 pre_checks[i][j]=checks[i][j];
     }
+    /**
+     * 撤回一步
+     */
     public void reset(){
         if (pre_checks!=null)
             for (int i=0;i<length;i++)
                 for (int j=0;j<length;j++)
                     checks[i][j]=pre_checks[i][j];
+                    //爆个错先，以免有笨蛋刚开始就想着撤回
         else throw new RuntimeException("no pre checks");
     }
-
+    //输出当前类的数据，仅用于调试时使用
     public void print(){
         System.out.println();
         System.out.println("check:");
@@ -286,7 +317,7 @@ public class Game {
         System.out.println();
         System.out.println("--------------------");
     }
-
+    //getter，简洁易懂
     public Integer getLength() {
         return length;
     }
@@ -300,6 +331,7 @@ public class Game {
     }
 
     //only for test
+    //久违的调试区，好啊就没动这里的代码了，当初就是用来debug一下，就一直留着了
     public static void main(String[] args) {
         Integer[][] chest=new Integer[][]{
                 {0,0,0,0},
